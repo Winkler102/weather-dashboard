e5cd9e6caba44724619c9d58604b7afa0f078f88
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-04-23 12:24:43 -0500

    Styled weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,44 @@
-let fetchWeatherData = function (city) {
+let cityHistory = [];
 
+let fetchWeatherData = function (city) {
+    console.log(city);
+
+    // fetch location Longitude and latitude
     fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=889b46e79741b547a80f6dcc38bdd6e3&units=imperial')
         .then(function (response) { return response.json(); })
         .then(function (response) {
             longitude = response.coord.lon;
             latitude = response.coord.lat;
 
+            // fetch weather information 
             fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=minutely,hourly&units=imperial&appid=889b46e79741b547a80f6dcc38bdd6e3')
                 .then(function (weather) { return weather.json(); })
                 .then(function (weather) {
-                    console.log(weather);
+
+                    // Place weather info into varibles
                     cityName = city;
                     temperature = weather.current.temp;
                     windSpeed = weather.current.wind_speed;
                     humidity = weather.current.humidity;
                     uvIndex = weather.current.uvi;
-
                     iconLink = "http://openweathermap.org/img/wn/" + weather.current.weather[0].icon + "@2x.png"
 
-                    iconCurrent = document.createElement('img');
-                    iconCurrent.src = iconLink;
-                    // $('#icon').append(iconCurrent);
+                    // Diplay weather info
+                    displayCurrent();
 
+                    // Get info for 7 day forecast by loop
                     for (i = 1; i < weather.daily.length; i++) {
-                        date = moment().add((i), 'days').format("MMMM Do, YYYY");
-                        iconLink = "http://openweathermap.org/img/wn/" + weather.daily[i].weather[0].icon + "@2x.png"
-                        temperatureWeek = weather.daily[i].temp.day;
-                        windSpeedWeek = weather.daily[i].wind_speed;
-                        humidityWeek = weather.daily[i].humidity;
-                        uvIndexWeek = weather.daily[i].uvi;
+                        dateWeek = moment().add((i), 'days').format("MMMM Do, YYYY");
+                        iconLinkWeek = "http://openweathermap.org/img/wn/" + weather.daily[i].weather[0].icon + "@2x.png"
+                        temperatureWeek = weather.daily[i].temp.day + " F";
+                        windSpeedWeek = weather.daily[i].wind_speed + " MPH";
+                        humidityWeek = weather.daily[i].humidity + " %";
+                        uvIndexWeek = "UV: " + weather.daily[i].uvi;
+                        iconImgWeek = document.createElement("img");
+                        iconImgWeek.classList.add("col-2");
+                        iconImgWeek.src = iconLinkWeek;
+                        weekInfo = [dateWeek, temperatureWeek, windSpeedWeek, humidityWeek, uvIndexWeek]
+                        addForecastList();
                     }
                 });
         });
@@ -36,10 +46,58 @@ let fetchWeatherData = function (city) {
 
 let displayDate = function () {
     currentDate = moment().format("MMMM Do, YYYY");
-    console.log(currentDate)
+    $('#dateCurrent').append(currentDate);
 }
 
-fetchWeatherData('Austin')
+let displayCurrent = function () {
+    $('#cityName').append(cityName);
+    $('#tempCurrent').append(temperature);
+    $('#windCurrent').append(windSpeed);
+    $('#humidCurrent').append(humidity);
+    $('#uviCurrent').append(uvIndex);
+
+
+    $('#iconCurrent')
+        .addClass('iconCurrentImg')
+        .attr('src', iconLink);
+
+}
+
+let addForecastList = function () {
+    $('#forecastList').append("<li class='row day" + i + "'></li>")
+
+    for (x = 0; x < weekInfo.length; x++) {
+        weekAddp = $('<p>');
+        weekAddp.addClass("col-2 p-3 m-0");
+        weekAddp.text(weekInfo[x]);
+        $(".day" + i).append(weekAddp);
+    }
+    $(".day" + i).append(iconImgWeek);
+    if ((i % 2) === 0) {
+        $(".day" + i).addClass('colored-background-light')
+    } else {
+        $(".day" + i).addClass('colored-background-dark')
+    }
+}
+
+let clearAll = function () {
+    $('#cityName').empty();
+    $('#tempCurrent').empty();
+    $('#windCurrent').empty();
+    $('#humidCurrent').empty();
+    $('#uviCurrent').empty();
+    $('#forecastList').empty();
+};
+
+let getCityName = function () {
+    cityNameInputEl = $("#cityNameInput").val();
+    cityHistory.push(cityNameInputEl)
+    console.log(cityHistory);
+    return cityNameInputEl;
+}
 displayDate();
 
-
+$("#cityNameForm").submit(function () {
+    clearAll();
+    fetchWeatherData(getCityName();)
+})
